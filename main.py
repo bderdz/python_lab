c72d80cd173f83656c9ec158fdfe76d09c880a4f
Bogdan Derdziak <bogdan.derdz@gmail.com>
2025-02-28 12:02:35 +0100

    lab 1 zadanie 6

--- main.py
+++ main.py
@@ -1,19 +1,53 @@
 from math import sin, cos, radians
 
 GRAVITY = 9.81
+DISTANCE = 100
+IMPACT_RADIUS = 5
 
-angle = None
 
-# if not (angle >= 0 and angle <= 90):
-#     print("Błędny kąt")
+def get_input():
+    angle = None
 
-while angle is None or not (angle >= 0 and angle <= 90):
-    angle = float(input("Podaj kąt: "))
+    while angle is None or not (angle >= 0 and angle <= 90):
+        angle = float(input("Podaj kąt: "))
 
-velocity = float(input("Podaj prędkość: "))
-# print(angle)
-# print(velocity)
-angle = radians(angle)
+    velocity = float(input("Podaj prędkość: "))
+    angle = radians(angle)
 
-z = (velocity ** 2 * sin(2 * angle)) / GRAVITY
-print(z)
+    return angle, velocity
+
+
+def calculate_impact(angle, velocity):
+    return (velocity ** 2 * sin(2 * angle)) / GRAVITY
+
+
+def check_victory(distance, z):
+    return abs(distance - z) < IMPACT_RADIUS
+
+
+def main():
+    player = 1
+
+    while True:
+        print(f"tura gracza {player}")
+        angle, velocity = get_input()
+        z = calculate_impact(angle, velocity)
+
+        if player == 1:
+            if (check_victory(DISTANCE, z)):
+                break
+        else:
+            z = DISTANCE - z
+
+            if (check_victory(0, z)):
+                break
+
+        print(z)
+
+        player = 3 - player
+
+    print(f"Zwyciężył gracz: {player}")
+
+
+if __name__ == '__main__':
+    main()
