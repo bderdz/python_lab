f7fbff804aa0209b59bee1d8b28c0b176b33a884
Bogdan Derdziak <bogdan.derdz@gmail.com>
2025-02-28 11:21:31 +0100

    lab 1 zadanie 4

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from math import sin, cos, radians
+
+GRAVITY = 9.81
+
 angle = None
 
 # if not (angle >= 0 and angle <= 90):
@@ -7,5 +11,9 @@ while angle is None or not (angle >= 0 and angle <= 90):
     angle = float(input("Podaj kąt: "))
 
 velocity = float(input("Podaj prędkość: "))
-print(angle)
-print(velocity)
+# print(angle)
+# print(velocity)
+angle = radians(angle)
+
+z = (velocity ** 2 * sin(2 * angle)) / GRAVITY
+print(z)
