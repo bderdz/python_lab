1b4b5eecdb0c05be78755eaafa464aad1b15cc4f
Bogdan Derdziak <bogdan.derdz@gmail.com>
2025-03-07 12:40:10 +0100

    lab 2 krok 6, 7

--- maps/main.py
+++ maps/main.py
@@ -1,6 +1,7 @@
 import curses
 
 PANEL_HEIGHT = 10
+barracks = None
 
 
 def load_structure_from_file(src):
@@ -11,11 +12,19 @@ def load_structure_from_file(src):
         return lines[0], lines[1:]
 
 
-def draw_structure(stdscr, structure):
+def draw_structure(stdscr, structure, x, y, centered=False, labeled=False, highlighted=False):
     name, art = structure
+    offset_y = y - len(art)
+    color = curses.color_pair(1 if highlighted else 0) | curses.A_BOLD
+
+    if centered:
+        x -= len(art[0]) // 2
 
     for i, line in enumerate(art):
-        stdscr.addstr(i, 0, line)
+        stdscr.addstr(offset_y + i, x, line, color)
+
+    if labeled:
+        stdscr.addstr(y + 1, x, name, color)
 
 
 def add_structure(structures, y, x, height):
@@ -33,15 +42,14 @@ def show_title_screen(stdscr, height, width):
         offset_x = (width - len(content)) // 2
         stdscr.addstr(offset_y + i, offset_x, content)
 
-    barracks = load_structure_from_file("structures/barracks.txt")
-    draw_structure(stdscr, barracks)
-
     stdscr.getch()
 
 
 def draw_map(stdscr, structures):
+    global barracks
+    
     for y, x in structures:
-        stdscr.addstr(y, x, "X")
+        draw_structure(stdscr, barracks, x, y, centered=True)
 
 
 def draw_separator(stdscr, height, width):
@@ -58,6 +66,11 @@ def draw_scene(stdscr, structures, height, width):
 
 
 def main(stdscr):
+    global barracks
+    barracks = load_structure_from_file("structures/barracks.txt")
+
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     curses.curs_set(0)
     curses.mousemask(curses.ALL_MOUSE_EVENTS | curses.REPORT_MOUSE_POSITION)
 
