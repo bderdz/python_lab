7779d32ec0939c104b06475635effcdf4a3e784d
Bogdan Derdziak <bogdan.derdz@gmail.com>
2025-03-21 11:03:54 +0100

    lab4 2a,2b

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 class Fraction:
-    numerator = None
-    denominator = None
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
 
     def is_integer(self):
         return self.numerator % self.denominator == 0
 
+    def __float__(self):
+        return float(self.numerator / self.denominator)
+
+    def __str__(self):
+        return f"{self.numerator}/{self.denominator}"
+
 
 def main():
-    fraction = Fraction()
-    fraction.numerator = 1
-    fraction.denominator = 2
+    fraction1 = Fraction(1, 2)
+    fraction2 = Fraction(4, 4)
 
-    fraction2 = Fraction()
-    fraction2.numerator = 4
-    fraction2.denominator = 4
+    print(fraction1)
+    print(fraction2)
 
-    print(f"{fraction.numerator}/{fraction.denominator}")
-    print(f"{fraction2.numerator}/{fraction2.denominator}")
+    print(float(fraction1))
 
-    print(f"1 - {fraction.is_integer()}")
+    print(f"1 - {fraction1.is_integer()}")
     print(f"2 - {fraction2.is_integer()}")
 
 
